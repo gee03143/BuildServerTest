5dde6bdc86facd9f6efe961c67f0c4d98f188d18
mingi.cho <mingi.cho@nhn.com>
2022-01-14 10:53:14 +0900

    Revert "Merge pull request #5 from gee03143/a2"
    This reverts commit db0f2e3f02d813fb4b208a1e3167cb6b5f726079, reversing
    changes made to f791382ab664a95be87030a9384de29949c0c4fa.

--- ShootingGame/Source/Tools/JenkinsFiles/BuildFullGame.groovy
+++ ShootingGame/Source/Tools/JenkinsFiles/BuildFullGame.groovy
@@ -1,27 +1,15 @@
 import java.text.SimpleDateFormat
 import groovy.json.JsonSlurperClassic
 
+
 node 
 {
-    withEnv([
-		"PLATFORM=${env.PLATFORM?env.PLATFORM:'Android'}",
-	])
+	stage('Stage 1')
 	{
-    	stage('Stage 1')
-    	{
-    		echo 'hello world!'
-    	}
-    	stage('Stage 2')
-    	{
-    		echo 'Stage 2'
-    	}
-    	stage('Stage 3')
-    	{
-    	    echo 'Stage 3'
-	    }
-	    stage('echo platform')
-	    {
-	        echo PLATFORM
-	    }
+		echo 'hello world!'
+	}
+	stage('Stage 2')
+	{
+		echo 'Stage 2'
 	}
 }
