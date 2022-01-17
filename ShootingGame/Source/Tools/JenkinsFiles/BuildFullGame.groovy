7e338b7d430f882f85a6073862471d30a7b29c4e
gee03143 <51311680+gee03143@users.noreply.github.com>
2022-01-17 17:16:04 +0900

    fix typo
    close double quotes

--- ShootingGame/Source/Tools/JenkinsFiles/BuildFullGame.groovy
+++ ShootingGame/Source/Tools/JenkinsFiles/BuildFullGame.groovy
@@ -16,7 +16,7 @@ node
 	}
 	stage('Build')
 	{
-		def buildCommandLine = "call ${UE4DIST_PATH}\\Engine\\Build\\BatchFiles\\RunUAT.bat BuildCookRun -project=\"${WORKSPACE}\\git\\BuildServerTest\\ShootingGame\\ShootingGame.uproject\" -clientconfig=Development -nodebuginfo -targetplatform=Win64 -cook -stage -pak
+		def buildCommandLine = "call ${UE4DIST_PATH}\\Engine\\Build\\BatchFiles\\RunUAT.bat BuildCookRun -project=\"${WORKSPACE}\\git\\BuildServerTest\\ShootingGame\\ShootingGame.uproject\" -clientconfig=Development -nodebuginfo -targetplatform=Win64 -cook -stage -pak"
 	}
 }
 
