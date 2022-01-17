f9e216801e2a11476ab58052452df646f01aa394
gee03143 <51311680+gee03143@users.noreply.github.com>
2022-01-17 17:53:25 +0900

    batch build command

--- ShootingGame/Source/Tools/JenkinsFiles/BuildFullGame.groovy
+++ ShootingGame/Source/Tools/JenkinsFiles/BuildFullGame.groovy
@@ -21,6 +21,7 @@ node
 	stage('Build')
 	{
 		def buildCommandLine = "call ${UE4DIST_PATH}\\Engine\\Build\\BatchFiles\\RunUAT.bat BuildCookRun -project=\"${WORKSPACE}\\git\\BuildServerTest\\ShootingGame\\ShootingGame.uproject\" -clientconfig=Development -nodebuginfo -targetplatform=Win64 -cook -stage -pak -map=Highrise -package"
+		bat buildCommandLine
 	}
 }
 
