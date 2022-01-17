f98ac5581659cf28407b4534611588f25f351682
gee03143 <51311680+gee03143@users.noreply.github.com>
2022-01-17 17:42:36 +0900

    add cbrace at mapname

--- ShootingGame/Source/Tools/JenkinsFiles/BuildFullGame.groovy
+++ ShootingGame/Source/Tools/JenkinsFiles/BuildFullGame.groovy
@@ -20,7 +20,7 @@ node
 	}
 	stage('Build')
 	{
-		def buildCommandLine = "call ${UE4DIST_PATH}\\Engine\\Build\\BatchFiles\\RunUAT.bat BuildCookRun -project=\"${WORKSPACE}\\git\\BuildServerTest\\ShootingGame\\ShootingGame.uproject\" -clientconfig=Development -nodebuginfo -targetplatform=Win64 -cook -stage -pak" -Map=Highrise.umap -package -archive -archivedirectory="${WORKSPACE}\\archive"
+		def buildCommandLine = "call ${UE4DIST_PATH}\\Engine\\Build\\BatchFiles\\RunUAT.bat BuildCookRun -project=\"${WORKSPACE}\\git\\BuildServerTest\\ShootingGame\\ShootingGame.uproject\" -clientconfig=Development -nodebuginfo -targetplatform=Win64 -cook -stage -pak" -Map={Highrise.umap} -package -archive -archivedirectory="${WORKSPACE}\\archive"
 	}
 }
 
